291a7985d838820f63be587df10a9c7ad9a00757
Redproletary <pavel.falin1992@rambler.ru>
2022-02-09 17:09:25 +0300

    Init 3

---
+++ Objects/CoordinateDemo.java
@@ -0,0 +1,11 @@
+package Objects;
+
+public class CoordinateDemo {
+    public static void main(String[] args) {
+        Coordinate coordinate = new Coordinate();
+        coordinate.setX(15);
+        coordinate.setY(25);
+        System.out.println(coordinate.getX());
+        System.out.println(coordinate.getY());
+    } // main(String[]) method
+} // CoordinateDemo class
