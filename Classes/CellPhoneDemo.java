3bd05dc1baba93c34ce6216ccfdbe8aa199c88ab
Redproletary <pavel.falin1992@rambler.ru>
2022-02-11 13:18:01 +0300

    Init 7

--- Classes/CellPhoneDemo.java
+++ Classes/CellPhoneDemo.java
@@ -6,7 +6,7 @@ public class CellPhoneDemo {
     public static void main(String[] args) {
         CellPhone cellPhone1 = new CellPhone("Pineapple", "piPhone Z");
         CellPhone cellPhone2 = new CellPhone("Pineapple", "piPhone Z");
-        if (cellPhone1 == cellPhone2) {
+        if (cellPhone1.equals(cellPhone2)) {
             System.out.println("телефоны одинаковые");
         }
     }
@@ -23,15 +23,9 @@ class CellPhone {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-        CellPhone cellPhone = (CellPhone) o;
-        return Objects.equals(vendor, cellPhone.vendor) &&
-                Objects.equals(model, cellPhone.model);
+        if (this == o) return true;
+        if (!(o instanceof CellPhone cellPhone)) return false;
+        return vendor.equals(cellPhone.vendor) && model.equals(cellPhone.model);
     }
 
     @Override
